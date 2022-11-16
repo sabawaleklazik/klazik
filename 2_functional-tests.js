59dce87cb753e829dfc661ec4338038198688384
sabawaleklazik <82668438+sabawaleklazik@users.noreply.github.com>
2022-11-16 12:47:33 +0100

    Update 2_functional-tests.js

--- 2_functional-tests.js
+++ 2_functional-tests.js
@@ -15,8 +15,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -24,10 +24,10 @@ suite('Functional Tests', function () {
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Sam')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Sam');
           done();
         });
     });
@@ -36,17 +36,27 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+      .send({ surname: 'Colombo'})
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo')
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
+      chai
+      .request(server)
+      .put('/travellers')
+      .send({surname: 'da Verrazzano'})
+      .end(function (err, res) {
+      assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano')
+      
       done();
     });
   });
